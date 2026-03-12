b84679faec937bd4cda1f6c8baf89fec9dc993de
OmarLawaty <omarlawatey@gmail.com>
2026-03-13 01:53:29 +0200

    feat: Add guards and decorators for route protection and current user

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,47 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { UsersService } from 'src/users';
+import { IS_PUBLIC_KEY } from 'src/auth';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const req = context.switchToHttp().getRequest();
+    const userId = req.session?.userId;
+
+    if (isPublic) {
+      if (userId)
+        req.currentUser = await this.usersService
+          .findOne(userId)
+          .catch(() => null);
+
+      return true;
+    }
+
+    if (!userId) throw new UnauthorizedException('Not authenticated');
+
+    const user = await this.usersService.findOne(userId);
+    if (!user) throw new UnauthorizedException('User not found');
+
+    req.currentUser = user;
+    req.session.user = user;
+
+    return true;
+  }
+}
