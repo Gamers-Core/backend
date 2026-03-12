fe69d6aa0551c8026825822843f6d4a838f63abf
OmarLawaty <omarlawatey@gmail.com>
2026-03-13 01:52:19 +0200

    feat: Integrate TypeORM with User entity and data source config

---
+++ datasource.ts
@@ -0,0 +1,41 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+import { join } from 'path';
+import { config } from 'dotenv';
+
+import { User } from 'src/entity';
+
+export const getDataSourceOptions = (): DataSourceOptions => {
+  config({
+    path: join(process.cwd(), `.env.${process.env.NODE_ENV ?? 'development'}`),
+  });
+
+  const migrations = [join(__dirname, 'migrations/*.{ts,js}')];
+
+  const dataSourceOptions: Partial<DataSourceOptions> = {
+    synchronize: false,
+    migrations,
+    entities: [User],
+  };
+
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      Object.assign(dataSourceOptions, {
+        type: 'sqlite',
+        database: 'db.sqlite',
+        synchronize: true,
+      });
+      break;
+    case 'staging':
+      break;
+    case 'production':
+      break;
+    default:
+      throw new Error('Unknown environment');
+  }
+
+  return dataSourceOptions as DataSourceOptions;
+};
+
+const dataSource = new DataSource(getDataSourceOptions());
+
+export default dataSource;
