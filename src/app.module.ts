912b9046fa2fa42fbed6e30b5ae59f112c994800
OmarLawaty <omarlawatey@gmail.com>
2026-03-13 01:51:33 +0200

    feat: Implement cookie-session management for user sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,59 @@
-import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import cookieSession from 'cookie-session';
+
+import { getDataSourceOptions } from 'datasource';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { AuthGuard } from './guards';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot(getDataSourceOptions()),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ whitelist: true }),
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    const cookieKey = this.configService.get<string>('COOKIE_KEY');
+    if (!cookieKey) throw new Error('COOKIE_KEY is required');
+
+    const isProduction =
+      this.configService.get<string>('NODE_ENV') === 'production';
+
+    consumer
+      .apply(
+        cookieSession({
+          keys: [cookieKey],
+          httpOnly: true,
+          sameSite: isProduction ? 'none' : 'lax',
+          secure: isProduction,
+        }),
+      )
+      .forRoutes('*');
+  }
+}
