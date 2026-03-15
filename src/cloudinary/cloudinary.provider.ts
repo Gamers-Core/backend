c1c400be04d4418283bad72f21d858dc9e167df8
OmarLawaty <omarlawatey@gmail.com>
2026-03-15 20:32:25 +0200

    feat: Refactor Cloudinary module and service for improved configuration handling

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -6,11 +6,14 @@ export const CLOUDINARY = 'CLOUDINARY';
 export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: (configService: ConfigService) => {
-    return cloudinary.config({
-      cloud_name: configService.get<string>('CLOUDINARY_CLOUD_NAME'),
-      api_key: configService.get<string>('CLOUDINARY_API_KEY'),
-      api_secret: configService.get<string>('CLOUDINARY_API_SECRET'),
-    });
+    const cloud_name = configService.get<string>('CLOUDINARY_CLOUD_NAME');
+    const api_key = configService.get<string>('CLOUDINARY_API_KEY');
+    const api_secret = configService.get<string>('CLOUDINARY_API_SECRET');
+
+    if (!cloud_name || !api_key || !api_secret)
+      throw new Error('Cloudinary configuration is missing.');
+
+    return cloudinary.config({ cloud_name, api_key, api_secret });
   },
   inject: [ConfigService],
 };
