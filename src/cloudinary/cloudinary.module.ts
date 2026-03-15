13044be84bd12abbf4aaf445653c4b27f14da24d
OmarLawaty <omarlawatey@gmail.com>
2026-03-15 20:32:25 +0200

    feat: Refactor Cloudinary module and service for improved configuration handling

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,14 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { Media } from 'src/entity';
 
 import { CloudinaryService } from './cloudinary.service';
 import { CloudinaryProvider } from './cloudinary.provider';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Media])],
   providers: [CloudinaryProvider, CloudinaryService],
   exports: [CloudinaryService],
 })
