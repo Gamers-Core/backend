6765f379be09a1485788f1ad4b8e4be35a2bbd33
OmarLawaty <omarlawatey@gmail.com>
2026-03-16 03:25:07 +0200

    feat: Add product creation DTO and endpoint wiring

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Product } from 'src/entity';
+import { MediaModule } from 'src/media';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), MediaModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
