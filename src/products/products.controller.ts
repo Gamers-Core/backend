91e9e320adb44bcba26ab9d03d563ab2e5070136
OmarLawaty <omarlawatey@gmail.com>
2026-03-16 06:04:57 +0200

    feat: Add ProductDTO and update products controller and service for enhanced product handling

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
-import { CreateProductDTO } from './dtos';
+import { Serialize } from 'src/interceptors';
+
+import { CreateProductDTO, ProductDTO } from './dtos';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @Serialize(ProductDTO)
   @Post()
   create(@Body() createProductDTO: CreateProductDTO) {
     return this.productsService.create(createProductDTO);
