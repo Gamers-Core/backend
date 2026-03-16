2071fab0e1d98038932911949c231cc0fe29b933
OmarLawaty <omarlawatey@gmail.com>
2026-03-16 06:06:30 +0200

    feat: Update media and product entities, refactor media upload handling, and introduce UploadMediaDTO

---
+++ src/media/dtos/upload-media.dto.ts
@@ -0,0 +1,19 @@
+import { IsIn, IsNotEmpty } from 'class-validator';
+
+import { mediaFolders } from 'src/cloudinary';
+import type { MediaFolder } from 'src/cloudinary';
+
+export class UploadMediaDTO {
+  @IsNotEmpty()
+  @IsIn(mediaFolders)
+  folder: MediaFolder;
+
+  file: {
+    fieldname: string;
+    originalname: string;
+    encoding: string;
+    mimetype: string;
+    buffer: Buffer;
+    size: number;
+  };
+}
