8755e67edd4c705b1d00b24accc810d6e3e62ba5
OmarLawaty <omarlawatey@gmail.com>
2026-04-03 08:01:38 +0200

    feat: Remove locale property from Request interface in express-request types

--- src/types/express-request.ts
+++ src/types/express-request.ts
@@ -1,9 +1,7 @@
 import type { User } from 'src/entity';
-import type { Locale } from 'src/i18n';
 
 declare module 'express-serve-static-core' {
   interface Request {
-    locale: Locale;
     user?: User;
     session?: {
       userId?: number;
