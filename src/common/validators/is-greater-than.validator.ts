286726acbf5ba838c156bdf258df52ccc6c9f53b
OmarLawaty <omarlawatey@gmail.com>
2026-04-04 21:19:53 +0200

    feat: Add IsGreaterThan validator and update exports in validators module

---
+++ src/common/validators/is-greater-than.validator.ts
@@ -0,0 +1,29 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export function IsGreaterThan(property: string, options?: ValidationOptions) {
+  return (object: object, propertyName: string) => {
+    registerDecorator({
+      name: 'isGreaterThan',
+      propertyName,
+      target: object.constructor,
+      constraints: [property],
+      options,
+      validator: {
+        validate(value: unknown, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+
+          if (!(relatedPropertyName in args.object)) return true;
+
+          const relatedValue = args.object[relatedPropertyName];
+
+          if (typeof value !== 'number' || typeof relatedValue !== 'number') return true;
+
+          return value > relatedValue;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be greater than ${args.constraints[0]}`;
+        },
+      },
+    });
+  };
+}
