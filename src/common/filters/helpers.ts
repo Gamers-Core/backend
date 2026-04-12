372cad727bb2bd84c936e8af3fc8361d0c8cee3a
OmarLawaty <omarlawatey@gmail.com>
2026-04-12 02:49:50 +0200

    feat: Implement translated messages in validation errors

--- src/common/filters/helpers.ts
+++ src/common/filters/helpers.ts
@@ -1,8 +1,18 @@
 import { ValidationError } from 'class-validator';
 
-export const formatErrors = (errors: ValidationError[]) =>
-  errors.map((error) => ({
-    property: error.property,
-    codes: Object.keys(error.constraints ?? {}),
-    children: error.children?.length ? formatErrors(error.children) : [],
-  }));
+import { I18nKey, Translate, translateWithoutLocale, type Locale } from 'src/i18n';
+
+export const formatErrors = (errors: ValidationError[] | undefined, locale: Locale) => {
+  if (!errors?.length) return [];
+
+  return errors.map((error) => {
+    const keys = Object.keys(error.constraints ?? {}) as Translate<I18nKey>[];
+
+    return {
+      property: error.property,
+      keys,
+      messages: keys.map(translateWithoutLocale(locale)),
+      children: formatErrors(error.children, locale),
+    };
+  });
+};
