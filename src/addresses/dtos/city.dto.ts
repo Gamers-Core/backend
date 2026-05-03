6007e91d3f079cc3c21d05589e7e3b986d3e19e2
OmarLawaty <omarlawatey@gmail.com>
2026-05-03 03:12:32 +0300

    feat: Create TODO file for app wide todos

--- src/addresses/dtos/city.dto.ts
+++ src/addresses/dtos/city.dto.ts
@@ -3,10 +3,13 @@ import { Expose } from 'class-transformer';
 export class CityDTO {
   @Expose()
   _id: string;
+
   @Expose()
   name: string;
+
   @Expose()
   nameAr: string;
+
   @Expose()
   code: string;
 }
--- src/addresses/dtos/district.dto.ts
+++ src/addresses/dtos/district.dto.ts
@@ -3,8 +3,10 @@ import { Expose } from 'class-transformer';
 export class DistrictDTO {
   @Expose()
   districtId: string;
+
   @Expose()
   districtName: string;
+
   @Expose()
   districtOtherName: string;
 }
