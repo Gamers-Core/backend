9fe164d4e9a4577a1d8ecd512c435a2909485f77
OmarLawaty <omarlawatey@gmail.com>
2026-05-07 15:48:38 +0300

    feat: Enhance IsAdminAuthGuard to set locale context for admin users

--- src/auth/guards/is-admin-auth.guard.ts
+++ src/auth/guards/is-admin-auth.guard.ts
@@ -2,15 +2,22 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 
 import { ForbiddenException, UnauthorizedException } from 'src/common/exceptions';
+import { defaultLocale } from 'src/i18n/const';
+import { LocaleContextService } from 'src/i18n/locale-context.service';
 
 @Injectable()
 export class IsAdminAuthGuard implements CanActivate {
+  constructor(private readonly localeContextService: LocaleContextService) {}
+
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest<Request>();
 
     if (!req.user) throw UnauthorizedException('unauthenticated');
 
     if (!req.user.isAdmin) throw ForbiddenException('unauthorized');
+
+    this.localeContextService.locale = defaultLocale;
+    req.res?.setHeader('x-locale', defaultLocale);
 
     return true;
   }
