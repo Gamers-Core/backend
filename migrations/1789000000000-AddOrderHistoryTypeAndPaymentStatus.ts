4a5b8cce651842a29e9fddbc915f88db64ee43d9
OmarLawaty <omarlawatey@gmail.com>
2026-05-27 00:28:37 +0300

    feat: Add checks for order status history type enums in migration; ensure types are created if not exist

--- migrations/1789000000000-AddOrderHistoryTypeAndPaymentStatus.ts
+++ migrations/1789000000000-AddOrderHistoryTypeAndPaymentStatus.ts
@@ -4,9 +4,19 @@ export class AddOrderHistoryTypeAndPaymentStatus1789000000000 implements Migrati
     name = 'AddOrderHistoryTypeAndPaymentStatus1789000000000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "order_status_history_type_enum" AS ENUM ('status', 'payment_status')`);
-        await queryRunner.query(`ALTER TABLE "order_status_history" ADD "type" "order_status_history_type_enum"`);
-        await queryRunner.query(`CREATE TYPE "order_status_history_status_enum" AS ENUM ('pending','confirmed','on-hold','on-progress','shipped','delivered','completed','returned','cancelled','unpaid','paid','refunded')`);
+                await queryRunner.query(`DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'order_status_history_type_enum') THEN
+        CREATE TYPE "order_status_history_type_enum" AS ENUM ('status', 'payment_status');
+    END IF;
+END$$;`);
+                await queryRunner.query(`ALTER TABLE "order_status_history" ADD COLUMN IF NOT EXISTS "type" "order_status_history_type_enum"`);
+                await queryRunner.query(`DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'order_status_history_status_enum') THEN
+        CREATE TYPE "order_status_history_status_enum" AS ENUM ('pending','confirmed','on-hold','on-progress','shipped','delivered','completed','returned','cancelled','unpaid','paid','refunded');
+    END IF;
+END$$;`);
         await queryRunner.query(`ALTER TABLE "order_status_history" ALTER COLUMN "status" TYPE "order_status_history_status_enum" USING "status"::text::"order_status_history_status_enum"`);
         await queryRunner.query(`UPDATE "order_status_history" SET "type" = 'status' WHERE "type" IS NULL`);
         await queryRunner.query(`ALTER TABLE "order_status_history" ALTER COLUMN "type" SET NOT NULL`);
