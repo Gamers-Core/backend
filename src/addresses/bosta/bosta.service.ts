b96d5667374d20acb3b1545044fd5e5c1a530548
OmarLawaty <omarlawatey@gmail.com>
2026-06-03 22:49:42 +0300

    refactor: Simplify BostaService by extending AxiosService and removing redundant code

--- src/addresses/bosta/bosta.service.ts
+++ src/addresses/bosta/bosta.service.ts
@@ -1,16 +1,16 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { InternalAxiosRequestConfig } from 'axios';
 
 import { ShippingFeesResponseDTO } from 'src/addresses/dtos/shipping-fees-response.dto';
 import { BostaPickupLocation } from 'src/addresses/types';
+import { AxiosService } from 'src/common/services/axios.service';
 import { ConfigService } from 'src/config/config.service';
 import { CacheService } from 'src/redis/cache.service';
 
 import { ShippingFeesDTO } from '../dtos/shipping-fees.dto';
 
-import { errorHandler, requestManager } from './helpers';
 import {
-  Instance,
   City,
   District,
   InsuranceFee,
@@ -19,36 +19,40 @@ import {
   DeliveryResponse,
   CreateDeliveryData,
   CreateDeliveryType,
+  BostaResponse,
+  BostaError,
 } from './types';
 
 @Injectable()
-export class BostaService {
-  private readonly bosta: Instance;
+export class BostaService extends AxiosService<BostaResponse<unknown>, BostaError> {
+  protected readonly baseURL = 'https://app.bosta.co/api/v2';
 
   constructor(
-    private readonly httpService: HttpService,
+    readonly httpService: HttpService,
     private readonly configService: ConfigService,
     private readonly cacheService: CacheService,
   ) {
-    const api = this.httpService.axiosRef.create({ baseURL: 'https://app.bosta.co/api/v2' });
+    super(httpService);
+  }
 
-    api.interceptors.request.use((config) => {
-      const token = this.configService.get('BOSTA_TOKEN');
+  protected onRequest(config: InternalAxiosRequestConfig) {
+    config.headers.Authorization = this.configService.get('BOSTA_TOKEN');
 
-      config.headers.Authorization = token;
+    return config;
+  }
 
-      return config;
-    });
+  protected extract<T>(wrapper: BostaResponse<unknown>): T {
+    return wrapper.data as T;
+  }
 
-    api.interceptors.response.use((res) => res, errorHandler);
-
-    this.bosta = requestManager(api);
+  protected extractError(err: BostaError): string {
+    return err.message ?? err.error;
   }
 
   getCities() {
     return this.cacheService.getOrSet<City[]>(
       'bosta:cities',
-      async () => await this.bosta.get<{ list: City[] }>('/cities').then((res) => res.data.list),
+      async () => await this.get<{ list: City[] }>('/cities').then((res) => res.list),
       { ttlMs: 1000 * 60 * 60 * 24 },
     );
   }
@@ -60,7 +64,7 @@ export class BostaService {
   getDistricts(cityId: string) {
     return this.cacheService.getOrSet<District[]>(
       `bosta:districts:${cityId}`,
-      async () => await this.bosta.get<District[]>(`/cities/${cityId}/districts`).then((res) => res.data),
+      async () => await this.get<District[]>(`/cities/${cityId}/districts`),
       { ttlMs: 1000 * 60 * 60 * 24 },
     );
   }
@@ -70,11 +74,7 @@ export class BostaService {
   }
 
   getInsuranceFees(goodsValue: number) {
-    return this.bosta
-      .get<InsuranceFee>('/pricing/insuranceFeeEstimate', {
-        params: { goodsValue },
-      })
-      .then((res) => res.data);
+    return this.get<InsuranceFee>('/pricing/insuranceFeeEstimate', { params: { goodsValue } });
   }
 
   async getShippingFees(params: ShippingFeesDTO): Promise<ShippingFeesResponseDTO> {
@@ -82,11 +82,9 @@ export class BostaService {
 
     const pickupCity = params.pickupCity ?? defaultPickupAddress?.address.city.name;
 
-    const { shippingFee, extraCodFee, tier } = await this.bosta
-      .get<ShippingFees>('/pricing/shipment/calculator', {
-        params: { ...params, cod: String(params.cod), size: 'Normal', type: 'SEND', pickupCity },
-      })
-      .then((res) => res.data);
+    const { shippingFee, extraCodFee, tier } = await this.get<ShippingFees>('/pricing/shipment/calculator', {
+      params: { ...params, cod: String(params.cod), size: 'Normal', type: 'SEND', pickupCity },
+    });
 
     return {
       shippingFee,
@@ -109,7 +107,7 @@ export class BostaService {
   getPickupLocations() {
     return this.cacheService.getOrSet<BostaPickupLocation[]>(
       'bosta:pickup-locations',
-      () => this.bosta.get<{ list: BostaPickupLocation[] }>('/pickup-locations').then((res) => res.data.list),
+      () => this.get<{ list: BostaPickupLocation[] }>('/pickup-locations').then((res) => res.list),
       { ttlMs: 1000 * 60 * 60 * 24 },
     );
   }
@@ -121,26 +119,24 @@ export class BostaService {
   async createDelivery(props: CreateDelivery) {
     const defaultPickupAddress = await this.getDefaultPickupLocation();
 
-    const delivery = await this.bosta
-      .post<DeliveryResponse, CreateDeliveryData>('/deliveries?apiVersion=1', {
-        businessLocationId: defaultPickupAddress._id,
-        type: CreateDeliveryType.DELIVER,
-        flexShippingInfo: {
-          amountToBeCollected: 200,
-          isOrderEligible: true,
-        },
-        notes: props.note,
-        cod: props.cod,
-        dropOffAddress: { cityId: props.cityId, districtId: props.districtId, firstLine: props.detailedAddress },
-        goodsInfo: { amount: props.unitPrice },
-        receiver: {
-          phone: props.phoneNumber,
-          fullName: props.nameAr,
-        },
-        businessReference: props.orderNumber,
-        allowToOpenPackage: props.canOpenPackage,
-      })
-      .then((res) => res.data);
+    const delivery = await this.post<DeliveryResponse, CreateDeliveryData>('/deliveries?apiVersion=1', {
+      businessLocationId: defaultPickupAddress._id,
+      type: CreateDeliveryType.DELIVER,
+      flexShippingInfo: {
+        amountToBeCollected: 200,
+        isOrderEligible: true,
+      },
+      notes: props.note,
+      cod: props.cod,
+      dropOffAddress: { cityId: props.cityId, districtId: props.districtId, firstLine: props.detailedAddress },
+      goodsInfo: { amount: props.unitPrice },
+      receiver: {
+        phone: props.phoneNumber,
+        fullName: props.nameAr,
+      },
+      businessReference: props.orderNumber,
+      allowToOpenPackage: props.canOpenPackage,
+    });
 
     return delivery;
   }
