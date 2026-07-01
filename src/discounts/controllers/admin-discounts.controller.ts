41938bc902841db9d6e38decdb1dbd846b6634d8
OmarLawaty <omarlawatey@gmail.com>
2026-07-01 04:18:05 +0300

    fix: Use correct dto serialization for admin discounts controller

--- src/discounts/controllers/admin-discounts.controller.ts
+++ src/discounts/controllers/admin-discounts.controller.ts
@@ -7,40 +7,46 @@ import { DiscountsService } from '../discounts.service';
 import { AdminDiscountDTO } from '../dtos/admin/admin-discount.dto';
 import { AdminSearchDiscountsDTO } from '../dtos/admin/admin-search-discounts.dto';
 import { CreateDiscountDTO } from '../dtos/admin/create-discount.dto';
+import { DiscountUsageDTO } from '../dtos/admin/discount-usage.dto';
 import { UpdateDiscountDTO } from '../dtos/admin/update-discount.dto';
 
 @Controller('admin/discounts')
 @UseGuards(IsAdminAuthGuard)
-@Serialize(AdminDiscountDTO)
 export class AdminDiscountsController {
   constructor(private readonly discountsService: DiscountsService) {}
 
   @Get()
+  @Serialize(AdminDiscountDTO)
   search(@Query() query: AdminSearchDiscountsDTO) {
     return this.discountsService.search(query);
   }
 
   @Get(':id')
+  @Serialize(AdminDiscountDTO)
   getOne(@Param('id', ParseIntPipe) id: number) {
     return this.discountsService.getOne(id);
   }
 
   @Post()
+  @Serialize(AdminDiscountDTO)
   add(@Body() body: CreateDiscountDTO) {
     return this.discountsService.add(body);
   }
 
   @Patch(':id')
+  @Serialize(AdminDiscountDTO)
   update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateDiscountDTO) {
     return this.discountsService.update(id, body);
   }
 
   @Delete(':id')
+  @Serialize(AdminDiscountDTO)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.discountsService.remove(id);
   }
 
   @Get(':id/usages')
+  @Serialize(DiscountUsageDTO)
   getUsages(@Param('id', ParseIntPipe) id: number) {
     return this.discountsService.getUsages(id);
   }
