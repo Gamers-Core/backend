5e1a88c880d4101faea30da65da785434fcd6c77
OmarLawaty <omarlawatey@gmail.com>
2026-07-31 17:54:31 +0300

    chore: Capture Bosta Webhook response

--- src/orders/controllers/bosta-orders.controller.ts
+++ src/orders/controllers/bosta-orders.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { deliveryStates } from 'src/addresses/bosta/const';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { WhatsAppService } from 'src/whatsapp/whatsapp.service';
 
 import { OrdersBostaWebhookDTO } from '../dtos/orders-bosta-webhook.dto';
 import { BostaWebhookAuthGuard } from '../guards/bosta-webhook.guard';
@@ -9,12 +10,19 @@ import { OrdersService } from '../services/orders.service';
 
 @Controller('bosta')
 export class BostaOrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    private readonly ordersService: OrdersService,
+    private readonly whatsappService: WhatsAppService,
+  ) {}
 
   @Public()
   @UseGuards(BostaWebhookAuthGuard)
   @Post('webhook')
-  handleWebhook(@Body() webhookData: OrdersBostaWebhookDTO) {
+  async handleWebhook(@Body() webhookData: OrdersBostaWebhookDTO) {
+    console.log(webhookData);
+
+    await this.whatsappService.sendText('01091226543', JSON.stringify(webhookData));
+
     const state = deliveryStates[webhookData.state];
 
     if (!state || (state === 'delivered' && !webhookData.isConfirmedDelivery) || !webhookData.trackingNumber) return;
