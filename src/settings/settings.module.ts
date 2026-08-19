d23460c4ecfff748db9fd949ad5f043f0539025e
OmarLawaty <omarlawatey@gmail.com>
2026-08-19 07:24:12 +0300

    feat: Implement setting media management with database migration and service updates

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { SettingMedia } from 'src/media/entities/setting-media.entity';
+import { MediaModule } from 'src/media/media.module';
+
 import { AdminSettingsController } from './controllers/admin-settings.controller';
 import { SettingsController } from './controllers/settings.controller';
 import { Setting } from './entities/setting.entity';
@@ -8,7 +11,7 @@ import { SettingsScheduler } from './settings.scheduler';
 import { SettingsService } from './settings.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Setting])],
+  imports: [TypeOrmModule.forFeature([Setting, SettingMedia]), MediaModule],
   controllers: [SettingsController, AdminSettingsController],
   providers: [SettingsService, SettingsScheduler],
   exports: [SettingsService],
